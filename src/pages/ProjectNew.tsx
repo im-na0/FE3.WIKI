a06c77c7bb264ce04f58672028472c0314a8f064
skyeome <tjru7230@naver.com>
2023-09-12 17:40:50 +0900

    Feat: 프로젝트 생성 페이지 마크업

--- src/pages/ProjectNew.tsx
+++ src/pages/ProjectNew.tsx
@@ -1,12 +1,20 @@
 import React from "react";
+import "../styles/Project.css";
+import ProjectSider from "../components/project/ProjectSider";
+// import { Layout, theme } from "antd";
+import { Layout } from "antd";
+import ProjectNewForm from "../components/project/ProjectNewForm";
 
-interface ProjectNewProps {
-  isEdit?: boolean;
-}
-
-const ProjectNew = ({ isEdit }: ProjectNewProps) => {
-  if (isEdit) return <div>ProjectEdit</div>;
-  else return <div>ProjectNew</div>;
+const ProjectNew = ({ isEdit }: { isEdit: boolean }) => {
+  // const {
+  //   token: { colorBgContainer, colorPrimary },
+  // } = theme.useToken();
+  return (
+    <Layout>
+      <ProjectSider />
+      <ProjectNewForm isEdit={isEdit} />
+    </Layout>
+  );
 };
 
 export default ProjectNew;
