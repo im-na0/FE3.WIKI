2046d1cc4011c322dcde695226e822468a89a4cd
NohWookJin <jinn98@kakao.com>
2023-09-13 14:50:05 +0900

    Feat: 메뉴 폴더, 파일 구조 생성 및 추가 기능

---
+++ src/components/Wiki/Page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// styles
+import styled from "styled-components";
+
+const Page = () => {
+  return (
+    <>
+      <h1>Wiki</h1>
+    </>
+  );
+};
+
+export default Page;
---
+++ src/components/Wiki/SubPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SubPage = () => {
+  return <div>SubPage</div>;
+};
+
+export default SubPage;
