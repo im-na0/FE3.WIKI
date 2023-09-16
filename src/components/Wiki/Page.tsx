d5138289c60b7d3ca4f9391af336833628d4fcf8
NohWookJin <jinn98@kakao.com>
2023-09-16 21:16:41 +0900

    Style: Width 비율 조절

--- src/components/Wiki/Page.tsx
+++ src/components/Wiki/Page.tsx
@@ -3,12 +3,19 @@ import React from "react";
 // styles
 import styled from "styled-components";
 
+// components
+import SubPage from "./SubPage";
+
 const Page = () => {
   return (
-    <>
-      <h1>Wiki</h1>
-    </>
+    <Container>
+      <SubPage />
+    </Container>
   );
 };
 
 export default Page;
+
+const Container = styled.div`
+  width: 100%;
+`;
