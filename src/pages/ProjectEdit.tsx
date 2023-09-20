88aab3da83c4bcc372a3d41aeadd57b70fffcabf
skyeome <tjru7230@naver.com>
2023-09-20 15:17:43 +0900

    Feat: 로딩시 스켈레톤 적용

--- src/pages/ProjectEdit.tsx
+++ src/pages/ProjectEdit.tsx
@@ -4,25 +4,28 @@ import ProjectSider from "../components/project/ProjectSider";
 // import { Layout, theme } from "antd";
 import { Layout } from "antd";
 import ProjectNewForm from "../components/project/ProjectNewForm";
+import ProjectNewFormSkeleton from "../components/project/ProjectNewFormSkeleton";
 import useQueryProjectEdit from "../hooks/project/useQueryProjectEdit";
 
 const ProjectEdit = ({ isEdit }: { isEdit: boolean }) => {
   const [teams, users, projectDetail, isLoaded] = useQueryProjectEdit();
   return (
     <>
-      {!isLoaded ? null : (
+      {isLoaded ? (
         <Layout>
           <ProjectSider />
-          {projectDetail !== undefined && (
+          {projectDetail !== undefined ? (
             <ProjectNewForm
               isEdit={isEdit}
               teams={teams}
               users={users}
               projectDetail={projectDetail}
             />
+          ) : (
+            <ProjectNewFormSkeleton />
           )}
         </Layout>
-      )}
+      ) : null}
     </>
   );
 };
