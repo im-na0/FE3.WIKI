9759ec00534bf0da00023a5aef2c7b931033e9c5
imnayoung <myblu1008@gmail.com>
2023-09-20 16:26:15 +0900

    Feat: 팀원 추가 시 이미 다른 팀에 속한 유저 중복 등록 방지

--- src/components/Employee/TeamMemberSelect.tsx
+++ src/components/Employee/TeamMemberSelect.tsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { Transfer, Form } from "antd";
+import { Transfer, Form, message } from "antd";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../libs/firebase";
 import { Rule } from "antd/lib/form";
 import CustomForm from "../common/CustomForm";
 import { userIdsState } from "../../store/member";
 import { useRecoilState } from "recoil";
+import { useTeamUserIds } from "../../hooks/Employee/useTeamUserIds";
 
 interface MemberSelectProps {
   onChange: (selectedUserIds: string[]) => void;
@@ -21,30 +22,41 @@ function TeamMemberSelect({ onChange }: MemberSelectProps) {
   const [users, setUsers] = useState<UserData[]>([]);
   const [selectedUserKeys, setSelectedUserKeys] = useState<string[]>([]);
   const [prevUserIds, setPrevUserIds] = useRecoilState(userIdsState);
+  const teamUserIds = useTeamUserIds(); // useTeamUserIds 훅을 사용하여 각 팀에 속한 사용자 ID를 가져옵니다.
 
   useEffect(() => {
     const fetchUsers = async () => {
-      const userCollectionRef = collection(db, "Users");
-      const userSnapshot = await getDocs(userCollectionRef);
-      const userArray: UserData[] = [];
+      try {
+        const userCollectionRef = collection(db, "Users");
+        const userSnapshot = await getDocs(userCollectionRef);
+        const userArray: UserData[] = [];
 
-      userSnapshot.forEach((doc) => {
-        const userData = { key: doc.id, title: doc.data().name || "" };
-        userArray.push(userData);
-      });
-      setUsers(userArray);
+        userSnapshot.forEach((doc) => {
+          const userData = { key: doc.id, title: doc.data().name || "" };
+          userArray.push(userData);
+        });
 
-      if (prevUserIds && prevUserIds.length > 0) {
-        const selectedKeys = userArray
-          .filter((user) => prevUserIds.includes(user.key))
-          .map((user) => user.key);
+        const filteredUserArray = userArray.filter(
+          (user) => !teamUserIds.includes(user.key),
+        );
 
-        setSelectedUserKeys(selectedKeys);
+        setUsers(filteredUserArray);
+
+        if (prevUserIds && prevUserIds.length > 0) {
+          const selectedKeys = userArray
+            .filter((user) => prevUserIds.includes(user.key))
+            .map((user) => user.key);
+
+          setSelectedUserKeys(selectedKeys);
+        }
+      } catch (error) {
+        console.error("Error fetching users:", error);
+        message.error("데이터를 불러올 수 없습니다!");
       }
     };
 
     fetchUsers();
-  }, [prevUserIds]);
+  }, [prevUserIds, teamUserIds]);
 
   const handleUserChange = (nextSelectedKeys: string[]) => {
     setSelectedUserKeys(nextSelectedKeys);
