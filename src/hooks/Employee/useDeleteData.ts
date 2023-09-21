a9a7486529d47f53a4d7e552a21cccc47a210845
imnayoung <myblu1008@gmail.com>
2023-09-21 18:54:33 +0900

    Fix: useDeleteData 훅 수정

--- src/hooks/Employee/useDeleteData.ts
+++ src/hooks/Employee/useDeleteData.ts
@@ -10,22 +10,19 @@ export function useDeleteData({ COLLECTION_NAME }: DeleteDataParams) {
   const deleteData = async (id: string, teamId?: string) => {
     try {
       await runTransaction(db, async (transaction) => {
-        // 유저 도큐먼트 참조
         const userDocRef = doc(db, COLLECTION_NAME, id);
 
         if (teamId) {
-          // 팀 도큐먼트 참조 및 데이터 가져오기
           const teamDocRef = doc(db, "Teams", teamId);
           const teamDoc = await transaction.get(teamDocRef);
 
-          // 팀 도큐먼트에 유저 ID가 있는 경우 제거
           if (teamDoc.exists() && teamDoc.data()?.userId?.includes(id)) {
             transaction.update(teamDocRef, {
               userId: arrayRemove(id),
             });
           }
         }
-        // 유저 삭제
+
         transaction.delete(userDocRef);
       });
 
